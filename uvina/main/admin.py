e72b814f63ca6ebd5639bff697d5074a607e3198
Tiago das Neves <61793991+tiagodasneves@users.noreply.github.com>
2023-02-17 09:57:10 -0300

    delete

--- uvina/main/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Pessoa, Instituicao, Peruca,Solicitacao, Cabelo
-admin.site.register(Pessoa)
-admin.site.register(Instituicao)
-admin.site.register(Peruca)
-admin.site.register(Solicitacao)
-admin.site.register(Cabelo)
-
-# Register your models here.
--- uvina/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .views import home_view
-
-urlpatterns = (
-    path('', views.home_view, name='home'),
-    path('cadastro/', views.cadastro, name='cadastro'),
-    path('login/', views.login, name='login')
-)
