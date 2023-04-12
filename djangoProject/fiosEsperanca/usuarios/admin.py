3ba6605246917013d024c662593dcecc9ab65ee8
Lauf <lucasafonsodesouza@gmail.com>
2023-04-12 00:36:29 -0300

    Exclui o outro projeto

--- djangoProject/fiosEsperanca/usuarios/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Pessoa
-from .models import Instituicao
-
-admin.site.register(Pessoa)
-admin.site.register(Instituicao)
-
-# Register your models here.
