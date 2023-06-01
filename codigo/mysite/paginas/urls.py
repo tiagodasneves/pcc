80e3a51a8693cbbbd982d488c8264b9e63ded7ef
enzo-afonso <ennzzoaff@gmail.com>
2023-05-31 23:03:27 -0300

    Testei a views

--- codigo/mysite/paginas/urls.py
+++ codigo/mysite/paginas/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('pagina/', views.teste, name='testando'),
 ]
--- codigo/mysite/paginas/views.py
+++ codigo/mysite/paginas/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'templates/index.html')
+
+def teste(request):
+    return HttpResponse("Estpu fazendo um teste!")
