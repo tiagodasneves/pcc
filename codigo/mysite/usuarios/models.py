e09e357fc89dd4393f49a381900ac67878b39465
enzo-afonso <ennzzoaff@gmail.com>
2023-06-01 00:19:48 -0300

    Tentativa de por o login

--- codigo/mysite/usuarios/models.py
+++ codigo/mysite/usuarios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Pessoa(models.Model):
     nome = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
+    senha = models.CharField(max_length=100)
     cpf = models.CharField(max_length=11)
     telefone = models.CharField(max_length=20)
     email = models.EmailField(max_length=255, unique=True)
--- codigo/mysite/usuarios/urls.py
+++ codigo/mysite/usuarios/urls.py
@@ -1,21 +1,7 @@
 from django.urls import path
 from . import views
+from .views import login
 
 urlpatterns = [
-    #urls de pessoa
-    path('cadastrar_pessoa/', views.cadastrar_pessoa, name='cadastrar_pessoa'),
-    path('editar_pessoa/<int:id>/', views.editar_pessoa, name='editar_pessoa'),
-    path('excluir_pessoa/<int:id>/', views.excluir_pessoa, name='excluir_pessoa'),
-    path('criar_depoimento/', views.criar_depoimento, name='criar_depoimento'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    #urls de depoimento
-    path('editar_depoimento/<int:pk>/', views.editar_depoimento, name='editar_depoimento'),
-    path('excluir_depoimento/<int:pk>/', views.excluir_depoimento, name='excluir_depoimento'),
-    path('listar_depoimentos/', views.listar_depoimentos, name='listar_depoimentos'),
-    #urls de endeço
-    path('criar_endereco/', views.criar_endereco, name='criar_endereco'),
-    path('editar_endereco/<int:id>/', views.editar_endereco, name='editar_endereco'),
-    path('excluir_endereco/<int:id>/', views.excluir_endereco, name='excluir_endereco'),
-    path('listar_enderecos/', views.listar_enderecos, name='listar_enderecos'),
+    path('login/', views.login, name='login'),
 ]
