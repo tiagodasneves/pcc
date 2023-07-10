59539d0c85875a911c0f1ea5c61ea27c1dc72dd2
enzo-afonso <ennzzoaff@gmail.com>
2023-07-10 19:48:34 -0300

    Terminado o app Usuarios

--- siteFDE/mysite/usuarios/urls.py
+++ siteFDE/mysite/usuarios/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name="home"),
     path('cadastro/', views.cadastro, name='cadastro'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.sair, name='logout'),
--- siteFDE/mysite/usuarios/views.py
+++ siteFDE/mysite/usuarios/views.py
@@ -2,18 +2,12 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from .models import Usuario
-from django.urls import reverse
+
+def home(request):
+    return render(request, "index.html")
 
 def cadastro(request):
     if request.method == 'POST':
-        #try:
-            #usuario_email = request.POST.get('email')
-            #usuario_username = request.POST.get('username')
-
-            #if usuario_email or usuario_username:
-                #return render(request, 'core/cadastro.html', {'msg': 'Erro! Já existe um usuário com o mesmo e-mail ou mesmo username'})
-            
-        #except User.DoesNotExist:
             username = request.POST.get('username')
             email = request.POST.get('email')
             nome = request.POST.get('nome')
@@ -52,7 +46,7 @@ def user_login (request):
 
         if usuario is not None:
             login(request, usuario)
-            return redirect(reverse('main:home'))
+            return redirect(home)
         else:
             return render(request, 'usuarios/login.html', {'msg': 'Credenciais inválidas!'})
 
@@ -62,15 +56,15 @@ def user_login (request):
 @login_required
 def sair(request):
     logout(request)
-    return redirect(reverse('main:home'))
+    return redirect(home)
 
 def ler_usuarios(request):
     usuarios = Usuario.objects.all()
-    return render(request, 'core/ler_usuarios.html', {"usuarios": usuarios})
+    return render(request, 'usuarios/ler_usuarios.html', {"usuarios": usuarios})
 
 def editar_usuario(request, id):
     usuario = Usuario.objects.get(id=id)
-    return render(request, 'core/editar_usuario.html', {"usuario": usuario})
+    return render(request, 'usuarios/editar_usuario.html', {"usuario": usuario})
 
 def atualizar_usuario(request, id):
     nome = request.POST.get('nome')
@@ -102,7 +96,7 @@ def deletar_usuario(request, id):
 
 def editar_perfil(request, id):
     usuario = Usuario.objects.get(id=id)
-    return render(request, 'core/editar_perfil.html', {"usuario": usuario})
+    return render(request, 'usuarios/editar_perfil.html', {"usuario": usuario})
 
 def atualizar_perfil(request, id):
     nome = request.POST.get('nome')
@@ -125,4 +119,4 @@ def atualizar_perfil(request, id):
     usuario.estado = estado
     usuario.save()
 
-    return redirect(reverse('main:home'))
+    return redirect(home)
