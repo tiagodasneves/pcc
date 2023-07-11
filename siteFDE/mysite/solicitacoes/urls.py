a7c8caf2a9233a47da5aff72b0d748465fcb207b
enzo-afonso <ennzzoaff@gmail.com>
2023-07-10 23:19:13 -0300

    Terminado com as perucas

--- siteFDE/mysite/solicitacoes/urls.py
+++ siteFDE/mysite/solicitacoes/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('cadastrar_peruca/', views.cadastrar_peruca, name='cadastrar_peruca'),
     path('listar_perucas/', views.listar_perucas, name='listar_perucas'),
+    path('editar_peruca/<int:pk>/', views.editar_peruca, name='editar_peruca'),
+    path('excluir_peruca/<int:pk>/', views.excluir_peruca, name='excluir_peruca'),
 ] 
--- siteFDE/mysite/solicitacoes/views.py
+++ siteFDE/mysite/solicitacoes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Peruca
 
 def cadastrar_peruca(request):
@@ -13,9 +13,33 @@ def cadastrar_peruca(request):
         
         return redirect('listar_perucas')
     
-    return render(request, 'solicitacoes/cadastrar_peruca.html')
+    return render(request, 'solicitacoes/perucas.html')
     
 def listar_perucas(request):
     perucas = Peruca.objects.all()
     context = {'perucas': perucas}
-    return render(request, 'solicitacoes/listar_perucas.html', context)
+    return render(request, 'solicitacoes/perucas.html', context)
+
+def editar_peruca(request, pk):
+    peruca = get_object_or_404(Peruca, pk=pk)
+    
+    if request.method == 'POST':
+        peruca.tamanho = request.POST['tamanho']
+        peruca.tipo = request.POST['tipo']
+        peruca.cor = request.POST['cor']
+        peruca.foto = request.FILES.get('foto')
+        peruca.save()
+        return redirect('listar_perucas')
+    
+    context = {'peruca': peruca}
+    return render(request, 'solicitacoes/editar_peruca.html', context)
+
+def excluir_peruca(request, pk):
+    peruca = get_object_or_404(Peruca, pk=pk)
+    
+    if request.method == 'POST':
+        peruca.delete()
+        return redirect('listar_perucas')
+    
+    context = {'peruca': peruca}
+    return render(request, 'solicitacoes/perucas.html', context)
